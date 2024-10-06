f80f560d37cdc9579e7e9362cde0894b06f8a161
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-06 07:15:20 -0300

    feat: implement user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, length: { in: 5..15 }, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 5..30 }, presence: true
 end
