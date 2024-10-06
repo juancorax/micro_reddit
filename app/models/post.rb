e3b58c43e3a4645aaa665a501d4990e6de9a37a6
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-06 07:55:41 -0300

    feat: set up associations between user & post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :title, length: { minimum: 5 }, presence: true
   validates :link, length: { minimum: 5 }, presence: true
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, length: { in: 5..15 }, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { in: 5..30 }, presence: true
+
+  has_many :posts
 end
